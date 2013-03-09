c1189430b7ea517c264d8bb4eb4248f11673638f
Matthieu Zimmer <contact@matthieu-zimmer.net>
2013-03-09 12:25:23 +0100

    use a small library of an old own project

---
+++ library/include/bib/StringUtils.hpp
@@ -0,0 +1,42 @@
+#ifndef STRINGUTILS_HPP
+#define STRINGUTILS_HPP
+
+///
+///\file StringUtils.hpp
+///\brief Quelques méthodes de manipulation des strings
+///\author matthieu
+///
+///
+
+#include <string>
+
+using namespace std;
+
+namespace bib {
+
+///
+///\brief Supprime tous les chiffres d'une chaîne de caractères
+///\param s : chaîne de départ
+///\return la chaîne sans chiffre
+///
+  
+string rmNumber(const string& s);
+
+///
+///\brief Supprime tous les premiers chiffres d'une chaîne de caractères
+///\param s : chaîne de départ
+///\return la chaîne sans ses premiers chiffre
+///
+
+string rmFirstNumber(const string& s);
+
+///
+///\brief regarde si la chaine de caractères ne contient que des espaces
+///\param s : chaîne de départ
+///\return true ou false
+///
+bool onlySpaceCharacter(const string& s);
+}
+
+
+#endif
